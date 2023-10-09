c94281978eb8674d65fe5b36adadb5a4154aa16c
RajibDesai <rajib.desai@gmail.com>
2023-10-10 00:45:39 +0600

    setup project

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,10 +1,38 @@
 import React from 'react';
+import './Header.css'
+import { Image } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 const Header = () => {
     return (
         <div>
-            <h1>this is header</h1>
-        </div>
+        <Navbar collapseOnSelect bg="primary" data-bs-theme="light" expand="lg">
+                <Container>
+                    <div>
+                        <Navbar.Brand>React-Bootstrap</Navbar.Brand>
+                            
+                    </div>
+
+                    <div>
+                        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                        <Navbar.Collapse id="responsive-navbar-nav">
+                            <Nav>
+                                <Nav.Link>More deets</Nav.Link>
+                                <Nav.Link >
+                                    Dank memes
+                                </Nav.Link>
+                                <Nav.Link>More deets</Nav.Link>
+                                <Nav.Link>
+                                    Dank memes
+                                </Nav.Link>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </div>
+                </Container>
+            </Navbar>
+            </div>
     );
 };
 
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC96BMX6pl8kulMpFKbD3DVDCHyMl4BdHc",
+  authDomain: "e-school-398cd.firebaseapp.com",
+  projectId: "e-school-398cd",
+  storageBucket: "e-school-398cd.appspot.com",
+  messagingSenderId: "990453946803",
+  appId: "1:990453946803:web:6f432c9e6b232bbe2f13c6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
+
